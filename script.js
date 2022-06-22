04d88b88f227da08f0e795cad6939e7b7f4db8ec
Mariia Butolina <104693230+butolinka@users.noreply.github.com>
2022-06-22 19:04:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from('.moveTop',{
+    opacity:0, x:200, stagger:1, duration:1.5
+})
+gsap.from('.about',{
+    scale:0.1, duration:3, delay:0.5, opacity:0
+})
+gsap.from('.firstrow',{
+    opacity:0, x:200, stagger:2, duration:3, delay:3.5
+})
+gsap.from('.secondrow',{
+    opacity:0, x:-200, stagger:2, duration:3, delay:3.5
+})
+
